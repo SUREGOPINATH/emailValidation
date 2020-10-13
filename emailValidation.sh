d4a1605229d843d954b2afc0103e14953f2020fc
SURE GOPINATH <suregopinath@gmail.com>
2020-10-13 19:01:39 +0530

    proccess of optional2 is done

--- emailValidation.sh
+++ emailValidation.sh
@@ -5,13 +5,13 @@ echo "welcome to eamil validation"
 echo "Enter the email"
 read email
 
-pat="[0-9a-zA-Z]+([-+_.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
+pat="^[0-9a-zA-Z]+([-+_.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
 
 if [[ $email =~ $pat ]]
 then
-        echo "valid"
+	echo "valid"
 else
-        echo "not valid"
+	echo "not valid"
 fi
 
 
