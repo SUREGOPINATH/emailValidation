3087e807607f49e368bef54961cd9230e189ff22
SURE GOPINATH <suregopinath@gmail.com>
2020-10-13 18:47:29 +0530

    process of mandatory3 is done

--- emailValidation.sh
+++ emailValidation.sh
@@ -5,7 +5,7 @@ echo "welcome to eamil validation"
 echo "Enter the email"
 read email
 
-pat="[0-9a-zA-Z]+@[0-9a-zA-Z]+"
+pat="[0-9a-zA-Z]+@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
 
 if [[ $email =~ $pat ]]
 then
