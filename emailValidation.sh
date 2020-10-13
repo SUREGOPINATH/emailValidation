ba374c3d695c67641c3bce365ed1744f8f42ccae
SURE GOPINATH <suregopinath@gmail.com>
2020-10-13 18:39:53 +0530

    process validation for mandatory1 is done

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 
 echo "welcome to eamil validation"
+
+echo "Enter the email"
+read email
+
+pat="[0-9a-zA-Z]+"
+
+if [[ $email =~ $pat ]]
+then
+        echo "valid"
+else
+        echo "not valid"
+fi
+
+
